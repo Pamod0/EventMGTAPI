9b2eecdfca08d656029cbbfd7351583f765e5c57
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2025-03-30 09:52:24 +0530

    repository pattern refactor

--- EventMGT/Interfaces/IEventUserRepository.cs
+++ EventMGT/Interfaces/IEventUserRepository.cs
@@ -6,5 +6,10 @@ namespace EventMGT.Interfaces
     public interface IEventUserRepository
     {
         Task<PagedResponse<List<EventUserDto>>> GetPagedUsersAsync(int page, int pageSize, string searchText, bool exactMatch);
+        Task<EventUser> GetUserByNicAsync(string nic);
+        Task<bool> IsUserRegisteredAsync(string nic);
+        Task<EventUser> AddUserAsync(EventUser user);
+        Task<EventUser> UpdateUserAsync(EventUser user);
+        Task<bool> SaveChangesAsync();
     }
 }
